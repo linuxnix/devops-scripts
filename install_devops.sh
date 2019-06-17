0b71095284dd408394909612fe6b60c50edaf228
Surendra Anne <kumarann@au1.ibm.com>
2019-06-17 13:28:24 +1000

    Added install_devops script

---
+++ install_devops.sh
@@ -0,0 +1,19 @@
+rpm -ivh http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+setenforce 0
+sed -i --follow-symlinks 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
+modprobe br_netfilter
+echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
+swapoff -a
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
+        https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+yum install -y kubelet kubeadm kubectl puppet anisble vim yum-utils device-mapper-persistent-data lvm2 docker-ce
+curl -L https://www.chef.io/chef/install.sh | sudo bash
